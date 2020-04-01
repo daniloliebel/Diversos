b5b3cb6062560d64efb542e80db02d432cabf317
daniloliebel <63000182+daniloliebel@users.noreply.github.com>
2020-04-01 13:21:52 -0300

    Snep
    Inicial

---
+++ guacamole/snep-asterisk/startMySql.sh
@@ -0,0 +1,13 @@
+file=`mysql -uroot -psneppass -e "SELECT count(*) as count  FROM information_schema.schemata where schema_name = 'snep'" | cut -d \t -f 2`
+if [ "$file" = "
+0" ];
+then
+	echo 'O sistema esta sendo iniciado pela primeira vez nesse ambiente, aguarde a inicializacao da base de dados.'
+	cd /var/www/html/snep/install/database
+	mysql -u root -psneppass < database.sql
+	mysql -u root -psneppass snep < schema.sql
+	mysql -u root -psneppass snep < system_data.sql
+	mysql -u root -psneppass snep < core-cnl.sql	
+else
+    echo 'O sistema ja esta na ultima versao do SNEP.'
+fi
